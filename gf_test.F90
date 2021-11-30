2bd56bb6ede1d7e3732fedfa6a2d769e95aa090b
Daniel Abdi <daniel.abdi@noaa.gov>
2021-11-30 02:54:42 -0700

    Initial commit

---
+++ gf_test.F90
@@ -0,0 +1,218 @@
+program test_gf
+   USE mt19937
+   USE gf_utils
+   USE cu_gf_driver
+   USE machine, only: kind_phys
+
+   IMPLICIT NONE
+
+   !--For init
+   integer, parameter  :: imfshalcnv = 2, imfshalcnv_gf = 2
+   integer, parameter  :: imfdeepcnv = 1, imfdeepcnv_gf = 1
+   integer, parameter  :: mpirank = 0
+   integer, parameter  :: mpiroot = 0
+   character(len=512)  :: errmsg
+   integer             :: errflg
+
+   !---For run
+   integer, parameter :: ix = 100, im = 100, km = 100, ntracer = 10
+   logical, parameter :: flag_for_scnv_generic_tend = .TRUE., flag_for_dcnv_generic_tend = .TRUE.
+   real (kind=kind_phys), parameter :: g = 1.0 ,cp = 2.0, xlv = 3.0, r_v = 2.3
+   logical, parameter :: ldiag3d = .TRUE.
+   real(kind=kind_phys) :: dt = 0.1
+   integer :: index_of_x_wind, index_of_y_wind, index_of_temperature,            &
+        index_of_process_scnv, index_of_process_dcnv, ntqv, ntcw, ntiw    !in
+
+   !allocatables
+   real(kind=kind_phys), allocatable :: dtend(:,:,:) !inout
+   integer, allocatable  :: dtidx(:,:) !in
+   real(kind=kind_phys),  dimension( : , : ), allocatable :: forcet,forceqv_spechum,w,phil !in
+   real(kind=kind_phys),  dimension( : , : ), allocatable :: t,us,vs !inout
+   real(kind=kind_phys),  dimension( : , : ), allocatable :: qci_conv !inout
+   real(kind=kind_phys),  dimension( : , : ), allocatable :: cnvw_moist,cnvc !out
+   real(kind=kind_phys),  dimension( : , : ), allocatable :: cliw, clcw !inout
+   integer, dimension (:),  allocatable :: hbot,htop,kcnv !out
+   integer, dimension (:),  allocatable :: xland !in
+   real(kind=kind_phys),    dimension (:), allocatable :: pbl !in
+   real(kind=kind_phys), dimension (:), allocatable    :: hfx2,qfx2,psuri !in
+   real(kind=kind_phys), dimension (:,:), allocatable  :: ud_mf,dd_mf,dt_mf !out
+   real(kind=kind_phys), dimension (:), allocatable    :: raincv,cld1d !out
+   real(kind=kind_phys), dimension (:,:), allocatable  :: t2di,p2di !in
+   real(kind=kind_phys), dimension (:,:), allocatable :: qv2di_spechum !in
+   real(kind=kind_phys), dimension (:,:), allocatable :: qv_spechum    !inout
+   real(kind=kind_phys), dimension(:), allocatable :: garea !in
+   integer, dimension(:), allocatable :: cactiv !inout
+
+   !---Allocating arrays
+   integer :: alloc_stat
+
+   PRINT*, "Allocating arrays"
+   ALLOCATE(                        &
+       garea(im),                   &
+       cactiv(im),                  & !integer
+       forcet(ix, km),              &
+       forceqv_spechum(ix, km),     &
+       phil(ix, km),                &
+       raincv(im),                  &
+       qv_spechum(ix, km),          &
+       t(ix, km),                   &
+       cld1d(im),                   &
+       us(ix, km),                  &
+       vs(ix, km),                  &
+       t2di(ix, km),                &
+       w(ix, km),                   &
+       qv2di_spechum(ix, km),       &
+       p2di(ix, km),                &
+       psuri(im),                   &
+       hbot(im),                    & !integer
+       htop(im),                    & !integer
+       kcnv(im),                    & !integer
+       xland(im),                   & !integer
+       hfx2(im),                    &
+       qfx2(im),                    &
+       cliw(ix, km),                &
+       clcw(ix, km),                &
+       pbl(im),                     &
+       ud_mf(im, km),               &
+       dd_mf(im, km),               &
+       dt_mf(im, km),               &
+       cnvw_moist(ix, km),          &
+       cnvc(ix, km),                &
+       dtend(im, km, 20),           & !confirm
+       dtidx(im, km),               & !integer
+       qci_conv(im, km),            & !confirm
+       STAT=alloc_stat)
+   IF (alloc_stat /= 0) STOP "Error allocating arrays"
+
+
+   !---Initialize arrays with random values
+   PRINT*, "Initialize arrays"
+   CALL mt19937_real1d(garea)
+   cactiv(:) = 1
+   CALL mt19937_real2d(forcet)
+   CALL mt19937_real2d(forceqv_spechum)
+   CALL mt19937_real2d(phil)
+   CALL mt19937_real1d(raincv)
+   CALL mt19937_real2d(qv_spechum)
+   CALL mt19937_real2d(t)
+   CALL mt19937_real1d(cld1d)
+   CALL mt19937_real2d(us)
+   CALL mt19937_real2d(vs)
+   CALL mt19937_real2d(t2di)
+   CALL mt19937_real2d(w)
+   CALL mt19937_real2d(qv2di_spechum)
+   CALL mt19937_real2d(p2di)
+   CALL mt19937_real1d(psuri)
+   hbot(:) = 1
+   htop(:) = 4
+   kcnv(:) = 1
+   xland(:) = 1
+   CALL mt19937_real1d(hfx2)
+   CALL mt19937_real1d(qfx2)
+   CALL mt19937_real2d(cliw)
+   CALL mt19937_real2d(clcw)
+   CALL mt19937_real1d(pbl)
+   CALL mt19937_real2d(ud_mf)
+   CALL mt19937_real2d(dd_mf)
+   CALL mt19937_real2d(dt_mf)
+   CALL mt19937_real2d(cnvw_moist)
+   CALL mt19937_real2d(cnvc)
+   CALL mt19937_real3d(dtend)
+   dtidx(:,:) = 1
+   CALL mt19937_real2d(qci_conv)
+
+   !--- Print state
+   CALL print_state("Input state",   &
+       garea,                   &
+       cactiv,                  &
+       forcet,                  &
+       forceqv_spechum,         &
+       phil,                    &
+       raincv,                  &
+       qv_spechum,              &
+       t,                       &
+       cld1d,                   &
+       us,                      &
+       vs,                      &
+       t2di,                    &
+       w,                       &
+       qv2di_spechum,           &
+       p2di,                    &
+       psuri,                   &
+       hbot,                    &
+       htop,                    &
+       kcnv,                    &
+       xland,                   &
+       hfx2,                    &
+       qfx2,                    &
+       cliw,                    &
+       clcw,                    &
+       pbl,                     &
+       ud_mf,                   &
+       dd_mf,                   &
+       dt_mf,                   &
+       cnvw_moist,              &
+       cnvc,                    &
+       dtend,                   &
+       dtidx,                   &
+       qci_conv                 &
+       )
+   !-------------
+
+   PRINT*, "Calling init"
+   CALL cu_gf_driver_init(imfshalcnv, imfshalcnv_gf, imfdeepcnv, &
+                          imfdeepcnv_gf,mpirank, mpiroot, errmsg, errflg)
+
+   PRINT*, "Calling run"
+   CALL cu_gf_driver_run(ntracer,garea,im,km,dt,cactiv,g,cp,xlv,r_v,   &
+               forcet,forceqv_spechum,phil,raincv,qv_spechum,t,cld1d,           &
+               us,vs,t2di,w,qv2di_spechum,p2di,psuri,                           &
+               hbot,htop,kcnv,xland,hfx2,qfx2,cliw,clcw,                        &
+               pbl,ud_mf,dd_mf,dt_mf,cnvw_moist,cnvc,imfshalcnv,                &
+               flag_for_scnv_generic_tend,flag_for_dcnv_generic_tend,           &
+               dtend,dtidx,ntqv,ntiw,ntcw,index_of_temperature,index_of_x_wind, &
+               index_of_y_wind,index_of_process_scnv,index_of_process_dcnv,     &
+               ldiag3d,qci_conv,errmsg,errflg)
+
+   PRINT*, "Calling finalize"
+   CALL cu_gf_driver_finalize()
+
+   !--- Print state
+   CALL print_state("Output state",   &
+       garea,                   &
+       cactiv,                  &
+       forcet,                  &
+       forceqv_spechum,         &
+       phil,                    &
+       raincv,                  &
+       qv_spechum,              &
+       t,                       &
+       cld1d,                   &
+       us,                      &
+       vs,                      &
+       t2di,                    &
+       w,                       &
+       qv2di_spechum,           &
+       p2di,                    &
+       psuri,                   &
+       hbot,                    &
+       htop,                    &
+       kcnv,                    &
+       xland,                   &
+       hfx2,                    &
+       qfx2,                    &
+       cliw,                    &
+       clcw,                    &
+       pbl,                     &
+       ud_mf,                   &
+       dd_mf,                   &
+       dt_mf,                   &
+       cnvw_moist,              &
+       cnvc,                    &
+       dtend,                   &
+       dtidx,                   &
+       qci_conv                 &
+       )
+   !-------------
+
+end program test_gf
---
+++ gf_utils.F90
@@ -0,0 +1,322 @@
+MODULE gf_utils
+
+  use machine , only : kind_phys
+
+  IMPLICIT NONE
+
+  PRIVATE
+  PUBLIC :: print_state
+
+CONTAINS
+
+  !------------------------------------------------------------------
+  ! print_state
+  !
+  ! Prints statistics for the kernel state variables
+  !------------------------------------------------------------------
+  SUBROUTINE print_state(msg,   &
+       garea,                   &
+       cactiv,                  &
+       forcet,                  &
+       forceqv_spechum,         &
+       phil,                    &
+       raincv,                  &
+       qv_spechum,              &
+       t,                       &
+       cld1d,                   &
+       us,                      &
+       vs,                      &
+       t2di,                    &
+       w,                       &
+       qv2di_spechum,           &
+       p2di,                    &
+       psuri,                   &
+       hbot,                    &
+       htop,                    &
+       kcnv,                    &
+       xland,                   &
+       hfx2,                    &
+       qfx2,                    &
+       cliw,                    &
+       clcw,                    &
+       pbl,                     &
+       ud_mf,                   &
+       dd_mf,                   &
+       dt_mf,                   &
+       cnvw_moist,              &
+       cnvc,                    &
+       dtend,                   &
+       dtidx,                   &
+       qci_conv                 &
+       )
+
+    CHARACTER(LEN=*) :: msg
+
+    REAL(kind_phys), INTENT(IN) ::       garea(:)
+    INTEGER, INTENT(IN) :: cactiv(:)
+    REAL(kind_phys), INTENT(IN) :: forcet(:, :)
+    REAL(kind_phys), INTENT(IN) :: forceqv_spechum(:, :)
+    REAL(kind_phys), INTENT(IN) :: phil(:, :)
+    REAL(kind_phys), INTENT(IN) :: raincv(:)
+    REAL(kind_phys), INTENT(IN) :: qv_spechum(:, :)
+    REAL(kind_phys), INTENT(IN) :: t(:, :)
+    REAL(kind_phys), INTENT(IN) :: cld1d(:)
+    REAL(kind_phys), INTENT(IN) :: us(:, :)
+    REAL(kind_phys), INTENT(IN) :: vs(:, :)
+    REAL(kind_phys), INTENT(IN) :: t2di(:, :)
+    REAL(kind_phys), INTENT(IN) :: w(:, :)
+    REAL(kind_phys), INTENT(IN) :: qv2di_spechum(:, :)
+    REAL(kind_phys), INTENT(IN) :: p2di(:, :)
+    REAL(kind_phys), INTENT(IN) :: psuri(:)
+    INTEGER, INTENT(IN) :: hbot(:)
+    INTEGER, INTENT(IN) :: htop(:)
+    INTEGER, INTENT(IN) :: kcnv(:)
+    INTEGER, INTENT(IN) :: xland(:)
+    REAL(kind_phys), INTENT(IN) :: hfx2(:)
+    REAL(kind_phys), INTENT(IN) :: qfx2(:)
+    REAL(kind_phys), INTENT(IN) :: cliw(:, :)
+    REAL(kind_phys), INTENT(IN) :: clcw(:, :)
+    REAL(kind_phys), INTENT(IN) :: pbl(:)
+    REAL(kind_phys), INTENT(IN) :: ud_mf(:, :)
+    REAL(kind_phys), INTENT(IN) :: dd_mf(:, :)
+    REAL(kind_phys), INTENT(IN) :: dt_mf(:, :)
+    REAL(kind_phys), INTENT(IN) :: cnvw_moist(:, :)
+    REAL(kind_phys), INTENT(IN) :: cnvc(:, :)
+    REAL(kind_phys), INTENT(IN) :: dtend(:, :, :)
+    INTEGER, INTENT(IN) :: dtidx(:, :)
+    REAL(kind_phys), INTENT(IN) :: qci_conv(:, :)
+
+    WRITE(*,'(A4)') "TEST"
+    WRITE(*,'(A5,A117)') "TEST ", REPEAT("=",117)
+    WRITE(*,'(A5,A32)') "TEST ", msg
+    WRITE(*,'(A5,A117)') "TEST ", REPEAT("=",117)
+    WRITE(*,'(A5,A17,5A20)') "TEST ", "Variable", "Min", "Max", "First", "Last", "RMS"
+    WRITE(*,'(A5,A117)') "TEST ", REPEAT("-",117)
+
+    CALL print_1d_variable("garea", garea)
+    CALL print_1d_variable_int("cactiv", cactiv)
+    CALL print_2d_variable("forcet", forcet)
+    CALL print_2d_variable("forceqv_spechum", forceqv_spechum)
+    CALL print_2d_variable("phil", phil)
+    CALL print_1d_variable("raincv", raincv)
+    CALL print_2d_variable("qv_spechum", qv_spechum)
+    CALL print_2d_variable("t", t)
+    CALL print_1d_variable("cld1d", cld1d)
+    CALL print_2d_variable("us", us)
+    CALL print_2d_variable("vs", vs)
+    CALL print_2d_variable("t2di", t2di)
+    CALL print_2d_variable("w", w)
+    CALL print_2d_variable("qv2di_spechum", qv2di_spechum)
+    CALL print_2d_variable("p2di", p2di)
+    CALL print_1d_variable("psuri", psuri)
+    CALL print_1d_variable_int("hbot", hbot)
+    CALL print_1d_variable_int("htop", htop)
+    CALL print_1d_variable_int("kcnv", kcnv)
+    CALL print_1d_variable_int("xland", xland)
+    CALL print_1d_variable("hfx2", hfx2)
+    CALL print_1d_variable("qfx2", qfx2)
+    CALL print_2d_variable("cliw", cliw)
+    CALL print_2d_variable("clcw", clcw)
+    CALL print_1d_variable("pbl", pbl)
+    CALL print_2d_variable("ud_mf", ud_mf)
+    CALL print_2d_variable("dd_mf", dd_mf)
+    CALL print_2d_variable("dt_mf", dt_mf)
+    CALL print_2d_variable("cnvw_moist", cnvw_moist)
+    CALL print_2d_variable("cnvc", cnvc)
+    CALL print_3d_variable("dtend", dtend)
+    CALL print_2d_variable_int("dtidx", dtidx)
+    CALL print_2d_variable("qci_conv", qci_conv)
+
+    WRITE(*,'(A5,A117)') "TEST ", REPEAT("-",117)
+    WRITE(*,'(A4)') "TEST"
+
+  END SUBROUTINE print_state
+
+  !------------------------------------------------------------------
+  ! print_1d_variable
+  !
+  ! Prints statistics for a 1d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_1d_variable(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all
+    ! dimensions
+    !       So we don't have to know start/end indices here
+    WRITE(*,'(A5, A17,5ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1), &
+                            data(SIZE(data,1)),            &
+                            SQRT(SUM(data**2) / SIZE(data))
+
+  END SUBROUTINE print_1d_variable
+
+  !------------------------------------------------------------------
+  ! print_2d_variable
+  !
+  ! Prints statistics for a 2d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_2d_variable(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all
+    ! dimensions
+    !       So we don't have to know start/end indices here
+    WRITE(*,'(A5, A17,5ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1), &
+                            data(SIZE(data,1), SIZE(data,2)),            &
+                            SQRT(SUM(data**2) / SIZE(data))
+
+  END SUBROUTINE print_2d_variable
+
+  !------------------------------------------------------------------
+  ! print_3d_variable
+  !
+  ! Prints statistics for a 3d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_3d_variable(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all dimensions
+    !       So we do not have to know start/end indices here
+    WRITE(*,'(A5,A17,5ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1,1),  &
+                            data(SIZE(data,1), SIZE(data,2), SIZE(data,3)), &
+                            SQRT(SUM(data**2) / SIZE(data))
+
+  END SUBROUTINE print_3d_variable
+
+  !------------------------------------------------------------------
+  ! print_4d_variable
+  !
+  ! Prints statistics for a 4d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_4d_variable(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:,:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all dimensions
+    !       So we do not have to know start/end indices here
+    WRITE(*,'(A5,A17,5ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1,1,1),  &
+                            data(SIZE(data,1), SIZE(data,2), SIZE(data,3), SIZE(data,4)), &
+                            SQRT(SUM(data**2) / SIZE(data))
+
+  END SUBROUTINE print_4d_variable
+
+
+  !------------------------------------------------------------------
+  ! print_5d_variable
+  !
+  ! Prints statistics for a 5d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_5d_variable(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:,:,:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all dimensions
+    !       So we do not have to know start/end indices here
+    WRITE(*,'(A5,A17,5ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1,1,1,1),  &
+                            data(SIZE(data,1), SIZE(data,2), SIZE(data,3), SIZE(data,4), SIZE(data,5)), &
+                            SQRT(SUM(data**2) / SIZE(data))
+
+  END SUBROUTINE print_5d_variable
+
+  !------------------------------------------------------------------
+  ! print_1d_variable
+  !
+  ! Prints statistics for a 1d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_1d_variable_int(name, data)
+
+    CHARACTER(LEN=*) :: name
+    INTEGER         :: data(:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all
+    ! dimensions
+    !       So we don't have to know start/end indices here
+    WRITE(*,'(A5, A17,4I20,ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1), &
+                            data(SIZE(data,1)),            &
+                            SQRT(REAL(SUM(data**2) / SIZE(data)))
+
+  END SUBROUTINE print_1d_variable_int
+
+  !------------------------------------------------------------------
+  ! print_2d_variable
+  !
+  ! Prints statistics for a 2d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_2d_variable_int(name, data)
+
+    CHARACTER(LEN=*) :: name
+    INTEGER         :: data(:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all
+    ! dimensions
+    !       So we don't have to know start/end indices here
+    WRITE(*,'(A5, A17,4I20,ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1), &
+                            data(SIZE(data,1), SIZE(data,2)),            &
+                            SQRT(REAL(SUM(data**2) / SIZE(data)))
+
+  END SUBROUTINE print_2d_variable_int
+
+  !------------------------------------------------------------------
+  ! print_3d_variable
+  !
+  ! Prints statistics for a 3d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_3d_variable_int(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all dimensions
+    !       So we do not have to know start/end indices here
+    WRITE(*,'(A5,A17,4I20,ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1,1),  &
+                            data(SIZE(data,1), SIZE(data,2), SIZE(data,3)), &
+                            SQRT(REAL(SUM(data**2) / SIZE(data)))
+
+  END SUBROUTINE print_3d_variable_int
+
+  !------------------------------------------------------------------
+  ! print_4d_variable
+  !
+  ! Prints statistics for a 4d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_4d_variable_int(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:,:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all dimensions
+    !       So we do not have to know start/end indices here
+    WRITE(*,'(A5,A17,4I20,ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1,1,1),  &
+                            data(SIZE(data,1), SIZE(data,2), SIZE(data,3), SIZE(data,4)), &
+                            SQRT(REAL(SUM(data**2) / SIZE(data)))
+
+  END SUBROUTINE print_4d_variable_int
+
+
+  !------------------------------------------------------------------
+  ! print_5d_variable
+  !
+  ! Prints statistics for a 5d state variable
+  !------------------------------------------------------------------
+  SUBROUTINE print_5d_variable_int(name, data)
+
+    CHARACTER(LEN=*) :: name
+    REAL(kind_phys)         :: data(:,:,:,:,:)
+
+    ! Note: Assumed shape array sections always start with index=1 for all dimensions
+    !       So we do not have to know start/end indices here
+    WRITE(*,'(A5,A17,4I20,ES20.10)') "TEST ", name, MINVAL(data), MAXVAL(data), data(1,1,1,1,1),  &
+                            data(SIZE(data,1), SIZE(data,2), SIZE(data,3), SIZE(data,4), SIZE(data,5)), &
+                            SQRT(REAL(SUM(data**2) / SIZE(data)))
+
+  END SUBROUTINE print_5d_variable_int
+
+
+END MODULE gf_utils
